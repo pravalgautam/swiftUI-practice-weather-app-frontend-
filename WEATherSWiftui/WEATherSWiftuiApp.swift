33a2b56c523d4e519a7e42b60e18d751803e978b
pravalgautam <pravalgautam79@gmail.com>
2023-03-12 19:40:32 +0530

    Initial Commit

---
+++ WEATherSWiftui/WEATherSWiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  WEATherSWiftuiApp.swift
+//  WEATherSWiftui
+//
+//  Created by Praval Gautam on 12/03/23.
+//
+
+import SwiftUI
+
+@main
+struct WEATherSWiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
