c83173039806ffaf4fdf36abcd7c32f6b7cb741a
pravalgautam <pravalgautam79@gmail.com>
2023-04-24 16:51:31 +0530

    fend

--- WEATherSWiftui/ContentView.swift
+++ WEATherSWiftui/ContentView.swift
@@ -38,7 +38,7 @@ struct ContentView: View {
                     weatherDay(dayOfWeek: "Tfree", imageName: "snow", Temperature: 76)
                     weatherDay(dayOfWeek: "TUEefe", imageName: "sun.max.fill", Temperature: 76)
                     weatherDay(dayOfWeek: "TUE", imageName: "wind.snow", Temperature: 76)
-                    weatherDay(dayOfWeek: "TUEd", imageName: "cloud.sun.fill", Temperature: 76)
+                    weatherDay(dayOfWeek: "TUEdsd", imageName: "cloud.sun.fill", Temperature: 76)
                  
                 }
                 Spacer()
